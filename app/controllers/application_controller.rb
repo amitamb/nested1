86fe817161aa9006ce9a71240217e79739a0aa7c
Amit Ambardekar <amitamb@gmail.com>
2022-08-18 13:26:32 +0530

    Basic company interface

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_company
+
+  def current_company
+    Company.first
+  end
 end
